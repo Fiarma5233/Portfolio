a17e1c587b7022035c3030e86115cdb423568bcc
SOME Fiarma Landry <SOME Fiarma Landry@fiarmasome@gmail.com>
2025-02-22 12:36:58 +0000

    Mise en forme de la page education avec pagination

--- skill/views.py
+++ skill/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from .models import  Experience
-
-# Create your views here.
+from django.core.paginator import Paginator
+from .models import Experience
 
 def skill(request):
-    experiences = Experience.objects.all().order_by('-year')  # Trier par année décroissante
+    # Récupérer toutes les expériences triées par année décroissante
+    experiences = Experience.objects.all().order_by('-year')
 
-    return render(request, 'skill/skill.html', {'experiences': experiences})
+    # Pagination : 4 expériences par page
+    paginator = Paginator(experiences, 4)
+    page_number = request.GET.get('page')  # Récupérer le numéro de page depuis l'URL
+    page_obj = paginator.get_page(page_number)  # Obtenir les objets de la page actuelle
+
+    return render(request, 'skill/skill.html', {'page_obj': page_obj})
