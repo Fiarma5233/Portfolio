b2ab0104f9c4db807de498115d2d90fa74f3d74e
SOME Fiarma Landry <SOME Fiarma Landry@fiarmasome@gmail.com>
2025-03-04 02:40:10 +0000

    update cloudinary

--- skill/models.py
+++ skill/models.py
@@ -29,6 +29,8 @@ class Experience(models.Model):
     year = models.CharField(max_length=50)  # Année d'obtention
     description = models.TextField()  # Description de la certification
     # certification_file = models.FileField(upload_to='certifications/', blank=True, null=True)  # Fichier de certification
-    certification_file = CloudinaryField('certifications', resource_type='auto', blank=True, null=True)
+    #certification_file = CloudinaryField('certifications', resource_type='auto', blank=True, null=True)
+    certification_file = CloudinaryField('certifications', resource_type='raw', format="pdf", blank=True, null=True)
+
     def __str__(self):
         return f"{self.title} - {self.organization}"
